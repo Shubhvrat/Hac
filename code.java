8898b5d214f626e2ae5e54e73c1820ae57a41210
Shubhvrat <shubhvratkulkarni@gmail.com>
2024-05-05 19:25:58 +0530

    //question1

--- code.java
+++ code.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * code
  */
@@ -5,6 +7,13 @@ public class code {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
