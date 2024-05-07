e60f758f2098d86c9a18a7f9aa830028246aa404
Shubhvrat <shubhvratkulkarni@gmail.com>
2024-05-07 21:44:09 +0530

    //changes

---
+++ ifelse.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+public class ifelse {
+      public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
